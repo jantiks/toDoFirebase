eaf0f44b1bfcf2b9f4d70419587d2ff3a213f8a0
jantiks <58886410+jantiks@users.noreply.github.com>
2020-12-29 11:09:34 +0400

    Initial Commit

---
+++ toDoFirebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  toDoFirebase
+//
+//  Created by Tigran on 12/29/20.
+//  Copyright © 2020 Tigran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
